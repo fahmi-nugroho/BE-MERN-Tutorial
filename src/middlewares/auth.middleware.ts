b9f74714f9c698a2ebff2e772507d79c9ed3485b
fahmi-nugroho <fahmingroho23@gmail.com>
2025-03-14 15:04:30 +0700

    feat: API Media Handling

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,41 +1,38 @@
-import {Request, Response, NextFunction} from "express";
-import {getUserData, IUserToken} from "../utils/jwt";
-
-export interface IReqUser extends Request {
-    user?: IUserToken
-}
+import { Request, Response, NextFunction } from "express";
+import { getUserData } from "../utils/jwt";
+import { IReqUser } from "../utils/interfaces";
 
 export default (req: Request, res: Response, next: NextFunction) => {
-    const authorization = req.headers?.authorization;
+  const authorization = req.headers?.authorization;
 
-    if (!authorization) {
-        res.status(403).json({
-            message: "unauthorized",
-            data: null
-        });
-        return
-    }
+  if (!authorization) {
+    res.status(403).json({
+      message: "unauthorized",
+      data: null,
+    });
+    return;
+  }
 
-    const [prefix, accessToken] = authorization.split(' ');
+  const [prefix, accessToken] = authorization.split(" ");
 
-    if (!(prefix === 'Bearer' && accessToken)) {
-        res.status(403).json({
-            message: "unauthorized",
-            data: null
-        });
-        return
-    }
+  if (!(prefix === "Bearer" && accessToken)) {
+    res.status(403).json({
+      message: "unauthorized",
+      data: null,
+    });
+    return;
+  }
 
-    const user = getUserData(accessToken)
+  const user = getUserData(accessToken);
 
-    if (!user) {
-        res.status(403).json({
-            message: "unauthorized",
-            data: null
-        });
-        return
-    }
+  if (!user) {
+    res.status(403).json({
+      message: "unauthorized",
+      data: null,
+    });
+    return;
+  }
 
-    (req as IReqUser).user = user
-    next()
-}
+  (req as IReqUser).user = user;
+  next();
+};
--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,15 +1,12 @@
-import {User} from "../models/user.model";
-import {Types} from "mongoose"
+import { User } from "../models/user.model";
+import { Types } from "mongoose";
 import jwt from "jsonwebtoken";
-import {SECRET} from "./env";
-
-export interface IUserToken extends Omit<User, 'password' | 'activationCode' | 'isActive' | 'email' | 'fullName' | 'profilePicture' | 'username'> {
-    id?: Types.ObjectId;
-}
+import { SECRET } from "./env";
+import { IUserToken } from "./interfaces";
 
 export const generateToken = (user: IUserToken) => {
-    return jwt.sign(user, SECRET, {expiresIn: '1h'});
-}
+  return jwt.sign(user, SECRET, { expiresIn: "1h" });
+};
 export const getUserData = (token: string) => {
-    return jwt.verify(token, SECRET) as IUserToken;
-}
+  return jwt.verify(token, SECRET) as IUserToken;
+};
