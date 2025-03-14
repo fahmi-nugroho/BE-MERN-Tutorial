ff0c5b30a245dda39511c6f27615a51db16166da
fahmi-nugroho <fahmingroho23@gmail.com>
2025-03-14 13:55:33 +0700

    feat: middleware control access

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,12 +1,25 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import authController from "../controllers/auth.controller";
-import authMiddleware from "../middlewares/auth.middleware"
+import authMiddleware from "../middlewares/auth.middleware";
+import aclMiddleware from "../middlewares/acl.middleware";
+import { ROLES } from "../utils/constant";
 
 const router = express.Router();
 
-router.post('/auth/register', authController.register)
-router.post('/auth/login', authController.login)
-router.get('/auth/me', authMiddleware, authController.me)
-router.post("/auth/activation", authController.activation)
+router.post("/auth/register", authController.register);
+router.post("/auth/login", authController.login);
+router.get("/auth/me", authMiddleware, authController.me);
+router.post("/auth/activation", authController.activation);
+
+router.get(
+  "/test-acl",
+  [authMiddleware, aclMiddleware([ROLES.ADMIN, ROLES.MEMBER])],
+  (req: Request, res: Response) => {
+    res.status(200).json({
+      data: "success",
+      message: "OK",
+    });
+  }
+);
 
 export default router;
